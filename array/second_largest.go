ebb1c4b2d410fd88e2f895b6cfdc19cec0ac7bd1
RijinSwaminathan <rijinswaminathan@gmail.com>
2025-06-17 07:54:15 +0530

    array based programs on first day

---
+++ array/second_largest.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	arr := []int{10, 10, 10}
+	max1 := math.MinInt
+	max2 := math.MinInt
+	for _, num := range arr {
+		if num > max1 {
+			max2 = max1
+			max1 = num
+		} else if num > max2 && num != max1 {
+			max2 = num
+		}
+	}
+	fmt.Println("Second largest number", max2)
+
+}
